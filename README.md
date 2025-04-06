b00404433edc981cd081ca86160c8c440bbe9454
Thiago Piffer Lauro <thiagopifferlauro@gmail.com>
2025-04-06 14:52:18 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Descrição do projeto AngularBlog
+
+Projeto feito acompanhando o BootCamp 2025 de Java e Angular da DIO, consistindo em um blog feito em Angular e visando aprimorar os conhecimentos no uso de componentes.
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVA SÉRIE SOBRE A HISTÓRIA DE KOGOS",
+    "description": "Netflix anuncia parceria com a Amazon para início de produção",
+    "photoCover":"https://i.ytimg.com/vi/HXrbNU39hq4/sddefault.jpg?v=61d375c9"
+  },
+  {
+    "id":"2",
+    "title": "Paulo Kogos vai em podcast nesse domingo",
+    "description": "bla blabla",
+    "photoCover":"https://media.gazetadopovo.com.br/2024/05/03111604/Paulo-Kogos.jpeg"
+  },
+  {
+    "id":"3",
+    "title": "Frases marcantes de Paulo Kogos",
+    "description": "bla blabla",
+    "photoCover":"https://i.ytimg.com/vi/uhNJgkk61-c/maxresdefault.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Paulo Kogos anuncia namoro e mulheres vão à loucura de ciúmes",
+    "description": "bla blabla",
+    "photoCover":"https://www.diariodocentrodomundo.com.br/wp-content/uploads/2020/11/maxresdefault-3.jpg"
+  }
+]
